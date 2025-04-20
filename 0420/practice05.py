8aef8e36414e0e3e994b9da8eaa093c784a752fc
South_Sea <tksz0613@gmail.com>
2025-04-20 16:15:03 +0900

    Initial commit

---
+++ 0420/practice05.py
@@ -0,0 +1,12 @@
+# print("정수를 입력해주세요 : ", end = "")
+# int a = input()
+
+a = int(input("정수를 입력해주세요 : "))
+b = int(input("정수를 입력해주세요 : "))
+print(a + b)
+
+a = [int(input("정수를 입력해주세요 : ")) for _ in range(2)]
+sum = 0
+for i in a:
+    sum += i
+print(sum)
