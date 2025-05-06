c192aab228ad51ed3441526540a6a4733276b08b
South_Sea <tksz0613@gmail.com>
2025-05-07 03:35:44 +0900

    feat : 삽입정렬 구현
    추가적으로 폴더명 변경

--- practice0506/practice02.py
+++ practice0506/practice02.py
@@ -2,13 +2,16 @@ import sys, os
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from __0505__ import practice01_simplified
+from practice0505 import practice01_simplified
 
 def sort_array(array):
-    for i in range(len(array) - 1):
-        for j in range(len(array) - 1 - i):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
+    for i in range(1, len(array)):
+        number = array[i]
+        for j in range(0, i):
+            if number < array[j]:
+                array[j + 1:i + 1] = array[j:i]
+                array[j] = number
+                break
     return array
 
 tmp = practice01_simplified.create_random_array_simplified(10)
